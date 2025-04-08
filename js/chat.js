77b4595213edf08aacf1c58f4881ccbe7f46503e
raghadalzalek <raghadza350@gmail.com>
2025-04-08 20:43:00 +0300

    إضافة مشروع

---
+++ js/chat.js
@@ -0,0 +1,9 @@
+window.$crisp = [];
+window.CRISP_WEBSITE_ID = "fbd5d83b-39f5-4447-884c-5acb4f4d82ae";
+(function () {
+    d = document;
+    s = d.createElement("script");
+    s.src = "https://client.crisp.chat/l.js";
+    s.async = 1;
+    d.getElementsByTagName("head")[0].appendChild(s);
+})();
